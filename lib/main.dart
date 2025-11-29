245306ed5d7648a6083458679bcfd3bfc91c13a8
Firmansyahadip <firmansyahadip2503@gmail.com>
2025-11-29 16:08:06 +0700

    solve conflict

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,12 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:critain/screens/perpustakaan-cerita.dart';
 import 'package:critain/screens/home_page.dart';
+import 'package:critain/screens/home_screen.dart';
+import 'package:critain/screens/dashboard.dart' as Dashboard;
+import 'package:critain/screens/maps_screen.dart';
+import 'package:critain/screens/mentor_screen.dart';
+import 'package:critain/screens/team_screen.dart';
+import 'package:critain/screens/testimonials_screen.dart';
 import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
@@ -37,6 +43,12 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const HomePage(),
         '/perpustakaan-cerita': (context) => const PerpustakaanCerita(),
+        '/home-screen': (context) => const HomeScreen(title: 'Home Screen'),
+        '/dashboard': (context) => const Dashboard.HomeScreen(title: 'Dashboard'),
+        '/maps': (context) => const MapsScreen(),
+        '/mentor': (context) => const MentorScreen(),
+        '/team': (context) => const TeamScreen(),
+        '/testimonials': (context) => const TestimonialsScreen(),
       },
     );
   }
